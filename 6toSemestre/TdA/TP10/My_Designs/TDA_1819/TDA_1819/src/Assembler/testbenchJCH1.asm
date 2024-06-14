3dfec617b18bb41a88157cd14418637c78a187bd
joacochanquia <143643776+joacochanquia@users.noreply.github.com>
2024-06-13 21:30:32 -0300

    Practica10TdA

---
+++ 6toSemestre/TdA/TP10/My_Designs/TDA_1819/TDA_1819/src/Assembler/testbenchJCH1.asm
@@ -0,0 +1,24 @@
+		.data
+A:		.hword	28877
+B:		.hword	6213
+C:		.hword	0xB5C9
+D:		.hword	0xAE
+E:		.hword	0
+F:		.hword	0
+G:		.hword	0
+H:		.hword	0
+		.code
+		lh r1, A(r0)
+		lh r6, B(r0)
+		lh r3, C(r0)
+		lh r8, D(r0)
+		dsubi r2, r1, 6213
+		dsubi r7, r6, 28877
+		xnorr r4, r2, r3
+		xnorr r9, r7, r8
+		sh r2, E(r0)
+		sh r7, G(r0)
+		sh r4, F(r0)
+		sh r9, H(r0)
+		halt
+	
